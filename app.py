c73805a541de9ee812b32c3bebcea54d57aaaca9
ppk <ppkywe27@gmail.com>
2024-09-11 10:40:16 +0700

    remove debug mode

--- app.py
+++ app.py
@@ -286,4 +286,4 @@ def predict_weather():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', debug=True, port=8888)
+    socketio.run(app, host='0.0.0.0', debug=False, port=8888)
